6ac53c8caeba41212153946d2a6764861ac1e73c
harsharshetty <harsha2071@gmail.com>
2026-09-14 09:17:40 +0530

    Align Subject with Lombok convention (#10)
    * Refactor Subject boilerplate to Lombok
    
    * Document Lombok boilerplate convention

--- backend/src/main/java/com/lumen/domain/Subject.java
+++ backend/src/main/java/com/lumen/domain/Subject.java
@@ -1,19 +1,27 @@
 package com.lumen.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "subjects")
 public class Subject {
-    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     @Column(nullable = false, unique = true)
     private String name;
 
-    protected Subject() {}
-    public Subject(String name) { this.name = name; }
-    public UUID getId() { return id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public Subject(String name) {
+        this.name = name;
+    }
 }
