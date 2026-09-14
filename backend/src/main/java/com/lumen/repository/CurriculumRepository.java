7d1fa433f22324a1efd5a2f6cbc350b6616c08da
harsharshetty <harsha2071@gmail.com>
2026-09-14 21:58:28 +0530

    Add active curriculum query

--- backend/src/main/java/com/lumen/repository/CurriculumRepository.java
+++ backend/src/main/java/com/lumen/repository/CurriculumRepository.java
@@ -3,7 +3,9 @@ package com.lumen.repository;
 import com.lumen.domain.Curriculum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CurriculumRepository extends JpaRepository<Curriculum, UUID> {
+    List<Curriculum> findAllByActiveTrue();
 }
