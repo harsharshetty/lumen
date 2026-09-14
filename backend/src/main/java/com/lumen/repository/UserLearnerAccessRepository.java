f3d2b40c084de1d691ffe4459f0a2862e2385d41
harsharshetty <harsha2071@gmail.com>
2026-09-14 20:10:41 +0530

    Add learner access repository

---
+++ backend/src/main/java/com/lumen/repository/UserLearnerAccessRepository.java
@@ -0,0 +1,18 @@
+package com.lumen.repository;
+
+import com.lumen.domain.Learner;
+import com.lumen.domain.User;
+import com.lumen.domain.UserLearnerAccess;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserLearnerAccessRepository extends JpaRepository<UserLearnerAccess, UUID> {
+    Optional<UserLearnerAccess> findByUserAndLearner(User user, Learner learner);
+
+    Optional<UserLearnerAccess> findByUserAndLearnerId(User user, UUID learnerId);
+
+    List<UserLearnerAccess> findAllByUser(User user);
+}
