5390db970c58d176ddf78f2eebbf908582b544c4
harsharshetty <harsha2071@gmail.com>
2026-09-14 08:46:22 +0530

    Refactor LearningConcept to Lombok

--- backend/src/main/java/com/lumen/domain/LearningConcept.java
+++ backend/src/main/java/com/lumen/domain/LearningConcept.java
@@ -1,21 +1,27 @@
 package com.lumen.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 import java.util.UUID;
 
 @Entity
 @Table(name = "learning_concepts")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RequiredArgsConstructor
 public class LearningConcept {
-    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     @Column(nullable = false, unique = true)
+    @NonNull
+    @Setter
     private String name;
-
-    protected LearningConcept() {}
-
-    public LearningConcept(String name) { this.name = name; }
-
-    public UUID getId() { return id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
 }
