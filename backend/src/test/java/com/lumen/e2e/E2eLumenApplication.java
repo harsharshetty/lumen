242c1ef59b10cb60819f03389837f0a051af4cf3
harsharshetty <harsha2071@gmail.com>
2026-09-15 06:42:38 +0530

    Discover repositories in E2E runtime

--- backend/src/test/java/com/lumen/e2e/E2eLumenApplication.java
+++ backend/src/test/java/com/lumen/e2e/E2eLumenApplication.java
@@ -5,11 +5,15 @@ import com.lumen.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
+@EntityScan(basePackages = "com.lumen.domain")
+@EnableJpaRepositories(basePackages = "com.lumen.repository")
 @ComponentScan(
         basePackages = "com.lumen",
         excludeFilters = {
