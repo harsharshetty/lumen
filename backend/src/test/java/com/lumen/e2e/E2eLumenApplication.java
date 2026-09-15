c76405363aa1b85be2a0f526a943e7271a6a42d0
harsharshetty <harsha2071@gmail.com>
2026-09-15 06:37:43 +0530

    Isolate E2E boot from production OAuth config

--- backend/src/test/java/com/lumen/e2e/E2eLumenApplication.java
+++ backend/src/test/java/com/lumen/e2e/E2eLumenApplication.java
@@ -1,13 +1,23 @@
 package com.lumen.e2e;
 
 import com.lumen.LumenApplication;
+import com.lumen.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
-public final class E2eLumenApplication {
-    private E2eLumenApplication() {
-    }
-
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan(
+        basePackages = "com.lumen",
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = LumenApplication.class),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        })
+public class E2eLumenApplication {
     public static void main(String[] args) {
-        new SpringApplication(LumenApplication.class, E2eSecurityConfiguration.class).run(args);
+        SpringApplication.run(E2eLumenApplication.class, args);
     }
 }
